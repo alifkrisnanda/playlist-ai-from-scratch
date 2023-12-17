82311af623a7ae2cc750d7be2b3216e95df81e73
alifkrisnanda <alifkrisnanda023@gmail.com>
2023-12-17 16:07:35 +0700

    add cnn and yolo

--- Multi Layer Perceptron/mlp.py
+++ Multi Layer Perceptron/mlp.py
@@ -1,236 +1,45 @@
-#%% video 1
-inputs = [1,2,3] #1 input
-weights = [0.2, 0.8, -0.5] #1 neuron
-bias = 2
-
-output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
-print(output)
-#%% video 2
-inputs = [1.0, 2.0, 3.0, 2.5]
-
-weights1 = [0.2, 0.8, -0.5, 1.0] #3 neuron
-weights2 = [0.5, -0.91, 0.26, -0.5]
-weights3 = [-0.26, -0.27, 0.17, 0.87] 
-
-bias1 = 2.0
-bias2 = 3.0
-bias3 = 0.5
-
-output = [inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + bias1,
-          inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2,
-          inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3]
-print(output)
-#%% video 3
-import numpy as np 
-
-# inputs = [1.0, 2.0, 3.0, 2.5]
-# weights = [[0.2, 0.8, -0.5, 1.0],
-#            [0.5, -0.91, 0.26, -0.5],
-#            [-0.26, -0.27, 0.17, 0.87]]
-
-# bias = [2.0, 3.0, 0.5]
-
-# output = np.dot(weights, inputs) + bias
-# print(output)
-
-inputs = np.array([1, 2, 3, 2.5])
-weights1 = np.array([0.2, 0.8, -0.5, 1.0])
-weights2 = np.array([0.5, -0.91, 0.26, -0.5])
-weights3 = np.array([-0.26, -0.27, 0.17, 0.87])
-
-# Combine weights into a single matrix
-weights_matrix = np.array([weights1, weights2, weights3])
-
-# bias
-bias = np.array([bias1, bias2, bias3])
-# Perform the dot product
-output = np.dot(inputs, weights_matrix.T) + bias  # Transpose the weights_matrix to match dimensions
-
-# Print the result
-print("Combined Weights Matrix:")
-print(weights_matrix)
-print("\nDot Product Result:")
-print(output)
-#%% video 4
-import numpy as np 
-
-np.random.seed(0)
-
-X = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
-
-
-class Layer_Dense:
-    def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
-        self.bias = np.zeros((1, n_neurons))
-    def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.bias
-
-layer1 = Layer_Dense(4,5)
-layer2 = Layer_Dense(5,2)
-
-layer1.forward(X)
-print("\nlayer 1 output:")
-print(layer1.output)
-layer2.forward(layer1.output)
-print("\nlayer 2 output:")
-print(layer2.output)
-
-#%% video 5
-import numpy as np 
-import nnfs
-from nnfs.datasets import spiral_data
-
-nnfs.init()
-
-X, y = spiral_data(100, 3)   
-
-
-class Layer_Dense:
-    def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1, n_neurons))
-    def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
-
-
-class Activation_ReLU:
-    def forward(self, inputs):
-        self.output = np.maximum(0, inputs)
-
-
-layer1 = Layer_Dense(2,5)
-activation1 = Activation_ReLU()
-
-layer1.forward(X)
-
-#print(layer1.output)
-activation1.forward(layer1.output)
-print(activation1.output)
-# %% video 6
-import numpy as np 
-import nnfs
-from nnfs.datasets import spiral_data
-
-
-nnfs.init()
-
-class Layer_Dense:
-    def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1, n_neurons))
-    def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
-
-
-class Activation_ReLU:
-    def forward(self, inputs):
-        self.output = np.maximum(0, inputs)
-
-class Activation_Softmax:
-    def forward(self, inputs):
-        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
-        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
-        self.output = probabilities
-
-
-X, y = spiral_data(samples=100, classes=3)
-
-dense1 = Layer_Dense(2,3)
-activation1 = Activation_ReLU()
-
-dense2 = Layer_Dense(3, 3)
-activation2 = Activation_Softmax()
-
-dense1.forward(X)
-activation1.forward(dense1.output)
-
-dense2.forward(activation1.output)
-activation2.forward(dense2.output)
-
-print(activation2.output[:5])
-#%% video 7
-import math
-
-softmax_output = [0.7, 0.1, 0.2]
-target_output = [1, 0, 0]
-
-loss = -(math.log(softmax_output[0]) * target_output[0] +
-         math.log(softmax_output[1]) * target_output[1] +
-         math.log(softmax_output[2]) * target_output[2])
-
-print(loss)
-
-print(-math.log(0.7))
-print(-math.log(0.5))
-#%% video 8
-import numpy as np 
-import nnfs
-from nnfs.datasets import spiral_data
-
-
-nnfs.init()
-
-class Layer_Dense:
-    def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1, n_neurons))
-    def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
-
-
-class Activation_ReLU:
-    def forward(self, inputs):
-        self.output = np.maximum(0, inputs)
-
-class Activation_Softmax:
-    def forward(self, inputs):
-        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
-        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
-        self.output = probabilities
-
-class Loss:
-    def calculate(self, output, y):
-        sample_losses = self.forward(output, y)
-        data_loss = np.mean(sample_losses)
-        return data_loss
-
-class Loss_CategoricalCrossentropy(Loss):
-    def forward(self, y_pred, y_true):
-        samples = len(y_pred)
-        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
-
-        if len(y_true.shape) == 1:
-            correct_confidences = y_pred_clipped[range(samples), y_true]
-
-        elif len(y_true.shape) == 2:
-            correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
-
-        negative_log_likelihoods = -np.log(correct_confidences)
-        return negative_log_likelihoods
-
-
-
-
-X, y = spiral_data(samples=100, classes=3)
-
-dense1 = Layer_Dense(2,3)
-activation1 = Activation_ReLU()
-
-dense2 = Layer_Dense(3, 3)
-activation2 = Activation_Softmax()
-
-dense1.forward(X)
-activation1.forward(dense1.output)
-
-dense2.forward(activation1.output)
-activation2.forward(dense2.output)
-
-print(activation2.output[:5])
-
-loss_function = Loss_CategoricalCrossentropy()
-loss = loss_function.calculate(activation2.output, y)
-
-print("Loss:", loss)
+#%% import dataset
+import pandas as pd
+import numpy as np
+from model import multilayer
+
+Data = pd.read_csv("iris.csv")
+#%% 
+X = Data.iloc[:, :-1].values
+y = Data.iloc[:, -1].values
+# 'setosa', 'versicolor', 'virginica'
+class_mapping = {label: index for index, label in enumerate(np.unique(y))}
+y = np.array([class_mapping[cls] for cls in y])
+#%% split dataset
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+datasets = np.array(Data)
+#%% 
+input_size = X.shape[1]  #jumlah fitur
+hidden_size = 8  # hidden_size yang diinginkan
+output_size = len(np.unique(y))  #jumlah class
+mlp = multilayer(input_size, hidden_size, output_size)
+mlp.train(X_train, y_train.reshape(-1, 1)) 
+#%% prediksi 
+predictions = mlp.predict(X_test)
+
+def threshold_predictions(predictions):
+    return np.argmax(predictions, axis=1)
+# print(predictions)
+# mengubah prediction ke predlabel
+predicted_labels = threshold_predictions(predictions)
+#%% akurasi 
+from sklearn.metrics import accuracy_score
+akurasi = accuracy_score(y_test, predicted_labels) * 100
+print(f"Accuracy: {akurasi:.2f}%")
+
+#%% plotting
+import matplotlib.pyplot as plt
+
+plt.scatter(X_train[:, 0], X_train[:, 1], marker="o", edgecolors="black", c=y_train, s=30, alpha=0.5, label='Training')
+plt.scatter(X_test[:, 0], X_test[:, 1], marker="x", c=y_test, s=100, label='Testing')
+plt.xlabel('Sepal length')
+plt.ylabel('sepal width')
+plt.legend()
+
+plt.show()
--- Single Layer Perceptron/uji_perceptron.py
+++ Single Layer Perceptron/uji_perceptron.py
@@ -5,13 +5,13 @@ Data = pd.read_csv('iris.csv')
 #%% Testing
 import numpy as np
 datasets = np.array(Data)
-Testing = pd.concat([Data[0:10],Data[50:60]])
+Testing = pd.concat([Data[0:5],Data[50:55]])
 Xtest = np.array(Testing[['sepal length (cm)', 'sepal width (cm)']])
-Ytest = np.concatenate((datasets[0:10,4],datasets[50:60,4]))
+Ytest = np.concatenate((datasets[0:5,4],datasets[50:55,4]))
 #%% Training
-Training = pd.concat([Data[10:50],Data[60:100]])
+Training = pd.concat([Data[5:50],Data[55:100]])
 Xtrain = np.array(Training[['sepal length (cm)', 'sepal width (cm)']])
-Ytrain = np.concatenate((datasets[10:50,4],datasets[60:100,4]))
+Ytrain = np.concatenate((datasets[5:50,4],datasets[55:100,4]))
 #%% 
 from Perceptron import perceptron
 
@@ -35,7 +35,7 @@ plt.ylabel('Sepal Width (cm)')
 x0_1 = np.amin(Xtrain[:, 0])
 x0_2 = np.amax(Xtrain[:, 0])
 
-x1_1 = (-p.weight[0]*x0_1 - p.ias)/p.weight[1]
+x1_1 = (-p.weight[0]*x0_1 - p.bias)/p.weight[1]
 x1_2 = (-p.weight[0]*x0_2 - p.bias)/p.weight[1]
 plt.plot([x0_1, x0_2], [x1_1, x1_2], "r")
 plt.show()
